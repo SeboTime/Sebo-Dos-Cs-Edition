27c28f38bfd82328fb8b44ca59d0a134f5884fec
SeboTime <sebochrisczemp@gmail.com>
2022-03-29 19:27:44 +0200

    Projektdateien hinzufügen.

---
+++ Sebo-Dos-Cs-Edition/Main.cs
@@ -0,0 +1,23 @@
+﻿while (true)
+{
+    string DosInput = Console.ReadLine();
+
+    string command0 = "help";
+    string command1 = "say";
+
+    if (DosInput == command0)
+    {
+        Console.WriteLine("Commands:");
+        Console.WriteLine(command0);
+        Console.WriteLine(command1);
+    }
+    else if (DosInput == command1)
+    {
+        Console.WriteLine(DosInput);
+    }
+    else
+    {
+        Console.WriteLine("{" + DosInput + "} is not a command!");
+    }
+    Console.WriteLine("");
+}
